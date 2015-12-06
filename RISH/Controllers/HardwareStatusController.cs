df4603533db9befabdde6baf3d61bdeebbb4a1e0
José Miguel Candia Hernández <jmcandia@gmail.com>
2015-12-06 16:25:44 -0300

    Más cambios

---
+++ RISH/Controllers/HardwareStatusController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RISH.Models;
+
+namespace RISH.Controllers
+{
+    public class HardwareStatusController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: HardwareStatus
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.HardwareStatuses.ToListAsync());
+        }
+
+        // GET: HardwareStatus/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HardwareStatus hardwareStatus = await db.HardwareStatuses.FindAsync(id);
+            if (hardwareStatus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hardwareStatus);
+        }
+
+        // GET: HardwareStatus/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HardwareStatus/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name")] HardwareStatus hardwareStatus)
+        {
+            if (ModelState.IsValid)
+            {
+                db.HardwareStatuses.Add(hardwareStatus);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(hardwareStatus);
+        }
+
+        // GET: HardwareStatus/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HardwareStatus hardwareStatus = await db.HardwareStatuses.FindAsync(id);
+            if (hardwareStatus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hardwareStatus);
+        }
+
+        // POST: HardwareStatus/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name")] HardwareStatus hardwareStatus)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hardwareStatus).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(hardwareStatus);
+        }
+
+        // GET: HardwareStatus/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HardwareStatus hardwareStatus = await db.HardwareStatuses.FindAsync(id);
+            if (hardwareStatus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hardwareStatus);
+        }
+
+        // POST: HardwareStatus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            HardwareStatus hardwareStatus = await db.HardwareStatuses.FindAsync(id);
+            db.HardwareStatuses.Remove(hardwareStatus);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RISH/Controllers/HardwareTypeController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RISH.Models;
+
+namespace RISH.Controllers
+{
+	[Authorize(Roles = "Administrador")]
+	public class HardwareTypeController : Controller
+	{
+		private ApplicationDbContext db = new ApplicationDbContext();
+
+		// GET: HardwareType
+		public async Task<ActionResult> Index()
+		{
+			return View(await db.HardwareTypes.ToListAsync());
+		}
+
+		// GET: HardwareType/Details/5
+		public async Task<ActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			HardwareType hardwareType = await db.HardwareTypes.FindAsync(id);
+			if (hardwareType == null)
+			{
+				return HttpNotFound();
+			}
+			return View(hardwareType);
+		}
+
+		// GET: HardwareType/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: HardwareType/Create
+		// Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+		// más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Create([Bind(Include = "Id,Name")] HardwareType hardwareType)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					db.HardwareTypes.Add(hardwareType);
+					await db.SaveChangesAsync();
+					return RedirectToAction("Index");
+				}
+			}
+			catch (DataException)
+			{
+				ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+			}
+			return View(hardwareType);
+		}
+
+		// GET: HardwareType/Edit/5
+		public async Task<ActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			HardwareType hardwareType = await db.HardwareTypes.FindAsync(id);
+			if (hardwareType == null)
+			{
+				return HttpNotFound();
+			}
+			return View(hardwareType);
+		}
+
+		// POST: HardwareType/Edit/5
+		// Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+		// más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Edit([Bind(Include = "Id,Name")] HardwareType hardwareType)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(hardwareType).State = EntityState.Modified;
+				await db.SaveChangesAsync();
+				return RedirectToAction("Index");
+			}
+			return View(hardwareType);
+		}
+
+		// GET: HardwareType/Delete/5
+		public async Task<ActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			HardwareType hardwareType = await db.HardwareTypes.FindAsync(id);
+			if (hardwareType == null)
+			{
+				return HttpNotFound();
+			}
+			return View(hardwareType);
+		}
+
+		// POST: HardwareType/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> DeleteConfirmed(int id)
+		{
+			HardwareType hardwareType = await db.HardwareTypes.FindAsync(id);
+			db.HardwareTypes.Remove(hardwareType);
+			await db.SaveChangesAsync();
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
+}
